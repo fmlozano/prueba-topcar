c4af82faa30e0de819ac9add0a1b0b24bbf10aa2
fmlozano <manololozano93@gmail.com>
2023-01-31 20:26:41 +0100

    feat(database/seeders): add seeders

---
+++ database/seeders/ContractSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ContractSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $file_path = resource_path('sql/contracts.sql');
+        DB::unprepared(file_get_contents($file_path));
+    }
+}
---
+++ database/seeders/VehicleSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class VehicleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $file_path = resource_path('sql/vehicles.sql');
+        DB::unprepared(file_get_contents($file_path));
+    }
+}
